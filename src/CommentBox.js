6a501d3db21aee9649ddfa19c69c1943effb3187
Ian MacIntosh <ianjmacintosh@gmail.com>
2016-08-31 22:42:58 -0400

    Build components to live inside CommentBox

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+class CommentList extends Component {
+  render() {
+    return (
+      <div className="commentList">
+        Hello world! I am a CommentList.
+      </div>
+    );
+  }
+}
+
+class CommentForm extends Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Bonjour monde! Je suis un CommentForm!
+      </div>
+    );
+  }
+}
+
 class CommentBox extends Component {
   render() {
     return (
       <div className="commentBox">
-        Hello world! I am a comment box.
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
