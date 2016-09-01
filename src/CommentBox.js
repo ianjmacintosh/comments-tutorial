1aef2a0b61bc90c269db34db9a1136a23b9c8c06
Ian MacIntosh <ianjmacintosh@gmail.com>
2016-08-31 22:36:43 -0400

    Modify boilerplate to match tutorial Delete irrelevant CSS -- we're starting from the ground up. Sort of. Lots of boilerplate, but deleting CSS is harmless, so doing that.

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class CommentBox extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import CommentBox from './CommentBox.js';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <CommentBox />,
   document.getElementById('root')
 );
