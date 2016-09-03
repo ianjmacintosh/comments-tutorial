0176a2689678aa3e66c91a79ff1bc17e0e01d63c
Ian MacIntosh <ianjmacintosh@gmail.com>
2016-09-03 14:53:25 -0400

    Move data into an object that gets passed into component Object then gets passed from one component to the next

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -3,10 +3,17 @@ import Remarkable from "remarkable";
 
 class CommentList extends Component {
   render() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+        );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
       /*
         > The <div> tags are not actual DOM nodes; they are instantiations of React div components.
@@ -34,7 +41,7 @@ class CommentBox extends Component {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data}/>
         <CommentForm />
       </div>
       /*
--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox.js';
 import './index.css';
 
+var data = [
+  {
+    "id": 1,
+    "author": "Pete Hunt",
+    "text": "This is one comment"
+  },
+  {
+    "id": 2,
+    "author": "Jordan Walke",
+    "text": "This is *another* comment"
+  }
+];
+
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('root')
 );
